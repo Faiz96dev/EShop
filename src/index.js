c20edf19ac438b0f7f264374f00b72f76ca87ec0
Faiz <faiz96dev@gmail.com>
2019-03-05 20:33:46 +0600

    hop

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import './main.css'
-import {createStore} from 'redux'
+import React from 'react'
+import ReactDom from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {browserHistory} from 'react-router'
+import {syncHistoryWidthStore} from 'react-router-redux'
+import reducers from 'reducers'
+import {Provider} from 'react-redux'
+import {Router, Route} from 'react-router'
 
-const store = createStore();
+import Layout from 'containers/layout'
+import Phones from 'containers/phones'  
 
-document.write('Hello React/Redux!')
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(thunk)
+));
+
+const history = syncHistoryWidthStore(browserHistory, store)
+
+
+ReactDom.render(
+    <Provider store={store}>
+    <Router history={History}>
+            <Route component={Layout}>
+            <Route path='/' component={Phones}>
+            
+            </Route>
+            </Route>
+    </Router>
+    </Provider>,
+
+    document.getElementById('root')
+)
